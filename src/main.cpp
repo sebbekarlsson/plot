37e1c6d915268dfe67b165ff2827a3474e88e2b6
Sebastian Karlsson <ianertson@gmail.com>
2021-08-10 23:20:39 +0200

    make cmake fetch freetype

--- src/main.cpp
+++ src/main.cpp
@@ -19,14 +19,14 @@ int main(int argc, char* argv[])
   plot->save("plot.bmp"); // can be saved as .bmp if you want
   plot->show();           // opens up a real-time window of your plot
 
-  plot->set_smooth(false);
+  plot->set_smooth(true);
 
   // Update the plot information while the plot window is showing.
   // The plot will be updated in real-time.
   float k = 0;
   while (1) {
     for (int i = 0; i < DATA_LEN; i++) {
-      data[i] = (perlin_get2d(k, i, 0.1f, 1, 23422));
+      data[i] = ((perlin_get2d(k, i, 0.1f, 1, 23422)) + ((perlin_get2d(i, k, 0.1f, 1, 4927)) * -1)) * cos(i+k);
     }
 
     plot->set_data(data, DATA_LEN);
